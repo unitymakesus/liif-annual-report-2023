d631048ce201fedaa6044affef906f1aa49dc43b
Andrew Marino <andrew@andrewmarino.net>
2023-11-20 15:03:52 -0500

    load typekit as stylesheet, fix console error

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -8,7 +8,8 @@
         <title>{{ $page->title }}</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Gothic+A1&family=Inter&family=Oswald&family=Readex+Pro&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.typekit.net/rcl7eij.css">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
         <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
     </head>
